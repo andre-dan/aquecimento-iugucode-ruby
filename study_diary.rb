62fde3e0ae13212d1eda9fd3f12a5e1413577392
andretorres <andrerndc@gmail.com>
2021-03-03 22:53:22 -0300

    Refatorando arquivo study_diary e Criando classe StudyItem

--- study_diary.rb
+++ study_diary.rb
@@ -1,49 +1,81 @@
-    def welcome
-        'Bem-vindo ao Diário de Estudos, seu companheiro para estudar!'
+  require_relative 'study_item'
+
+  INSERT = 1
+  PRINT = 2
+  SEARCH = 3
+  EXIT = 4
+
+  def welcome
+    'Bem vindo ao Diário de Estudos, seu companheiro para Estudar !'
+  end
+
+  def thanks
+    puts 'Obrigado por usar o Diário de Estudos'
+  end 
+
+  def clear  
+    system('clear')
+  end
+
+  def wait_keypress_and_clear 
+    puts
+    puts "Pressione enter para retornar ao menu"
+    gets
+    clear
+  end
+
+  def menu
+    puts "[#{INSERT}] - Para Cadastrar um item para estudar"
+    puts "[#{PRINT}] - Ver Todos os itens cadastrados"
+    puts "[#{SEARCH}] - Buscar um item de estudo"
+    puts "[#{EXIT}] - Sair"
+    print "Escolha sua opção: "
+    gets.to_i
+  end
+
+  def register_study_item
+    print 'Digite o título do seu item de estudo: '
+    title = gets.chomp
+    print 'Digite a categoria do seu item de estudo: '
+    category = gets.chomp
+    puts "Item '#{title}' da categoria '#{category}' cadastrado com sucesso!"
+    StudyItem.new(title, category)
+  end
+
+  def print_items(collection)
+    puts "=========== Itens Cadastrados =========== "  
+    puts "Nenhum Item cadastrado" if collection.empty?
+    collection.each_with_index do |item, index|
+      puts "# #{index + 1} - #{item.title} - #{item.category}"
     end
-     ​
-    def menu
-        puts "[1] Cadastrar um item para estudar"
-        puts "[2] Ver todos os itens cadastrados"
-        puts "[3] Buscar um item de estudo"
-        puts "[4] Sair"
-        print 'Escolha uma opção: '
-        gets.to_i
-    end
-    ​
-    def register_study_item
-        print 'Digite o título do seu item de estudo: '
-        title = gets.chomp
-        print 'Digite a categoria do seu item de estudo: '
-        category = gets.chomp
-        puts "Item '#{title}' da categoria '#{category}' cadastrado com sucesso!"
-        { title: title, category: category }
-    end
-    ​
-    puts welcome
-    study_items = []
-    option = menu
-    ​
-    while option != 4
-        if option == 1
+  end
+
+  def search_item(collection)
+    print 'Digite uma palavra para buscar: '
+    term = gets.chomp
+    found_items = collection.filter do |item|
+      item.title.include? term
+  end
+    print_items(found_items)
+  end
+  clear
+  study_items = []
+  puts welcome
+  option = menu
+
+    loop do
+      clear
+      if option == INSERT    
         study_items << register_study_item
-        elsif option == 2
-        study_items.each_with_index do |item, index|
-            puts "##{index + 1} - #{item[:title]} - #{item[:category]}"
-        end
-        puts 'Nenhum item cadastrado' if study_items.empty?
-        elsif option == 3
-        print 'Digite uma palavra para procurar: '
-        term = gets.chomp
-        found_items = study_items.filter do |item|
-            item[:title].include? term
-        end
-        puts found_items
-        puts 'Nenhum item encontrado' if found_items.empty?
-        else
-        puts 'Opção inválida'
-        end
+      elsif option == PRINT
+        print_items(study_items)
+      elsif option == SEARCH
+        search_item(study_items)
+      elsif option == EXIT
+        break
+      end
+        wait_keypress_and_clear
         option = menu
     end
-    ​
-    puts 'Obrigado por usar o Diário de Estudos'
+    
+  thanks
---
+++ study_item.rb
@@ -0,0 +1,8 @@
+class StudyItem
+  attr_accessor :title, :category
+
+  def initialize(title, category)
+    @title = title
+    @category = category
+  end
+end
